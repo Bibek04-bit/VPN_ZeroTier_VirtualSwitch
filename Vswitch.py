8ab5237f57b68d6472400f14553c9f1054cd0262
Bibek Acharya <92309375+Bibek04-bit@users.noreply.github.com>
2025-09-21 10:21:30 +0545

    updated

--- Vswitch.py
+++ Vswitch.py
@@ -1,3 +1,15 @@
 import socket
 import sys
 
+server_port = None
+if len(sys.argv)!=2:
+  print("Usage: python3 vswitch.py {VSWITCH_PORT}")
+  sys.exit(1)
+else:
+  server_port = int(sys.argv[1])
+server_addr = ("0.0.0.0", server_port)
+
+
+
+
+
